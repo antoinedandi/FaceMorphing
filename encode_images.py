116024c6c327daf4a899efa2340708cdb529c756
antoinedandi <antoine.dandigne@gmail.com>
2020-02-20 18:56:29 +0100

    refactoring of encode_images script

--- encode_images.py
+++ encode_images.py
@@ -11,7 +11,7 @@ from utils.utils import split_to_batches
 from encoder.generator_model import Generator
 from encoder.perceptual_model import PerceptualModel, load_images
 from keras.models import load_model
-
+from keras.applications.resnet50 import preprocess_input
 
 # Pretrained models URL
 url_styleGAN = 'https://drive.google.com/uc?export=download&id=1Ru1kpacSvmheTHP7evEGHEegXZjeTaoi'
@@ -21,27 +21,25 @@ url_VGG_perceptual = 'https://drive.google.com/uc?export=download&id=1poMANPSNDH
 
 def main():
     parser = argparse.ArgumentParser(description='Find latent representation of reference images using perceptual losses', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    # Output directories setting
     parser.add_argument('src_dir', help='Directory with images for encoding')
     parser.add_argument('generated_images_dir', help='Directory for storing generated images')
     parser.add_argument('guessed_images_dir', help='Directory for storing initially guessed images')
     parser.add_argument('dlatent_dir', help='Directory for storing dlatent representations')
-    parser.add_argument('--data_dir', default='data', help='Directory for storing optional models')
-    parser.add_argument('--mask_dir', default='masks', help='Directory for storing optional masks')
-    parser.add_argument('--load_last', default='', help='Start with embeddings from directory')
-    parser.add_argument('--dlatent_avg', default='', help='Use dlatent from file specified here for truncation instead of dlatent_avg from Gs')
-    parser.add_argument('--model_url', default=url_styleGAN, help='Fetch a StyleGAN model to train on from this URL') # karras2019stylegan-ffhq-1024x1024.pkl
+
+    # General params
     parser.add_argument('--model_res', default=1024, help='The dimension of images in the StyleGAN model', type=int)
     parser.add_argument('--batch_size', default=1, help='Batch size for generator and perceptual model', type=int)
+    parser.add_argument('--use_resnet', default=True, help='Use pretrained ResNet for approximating dlatents', type=lambda x: (str(x).lower() == 'true'))
 
     # Perceptual model params
-    parser.add_argument('--image_size', default=256, help='Size of images for perceptual model', type=int)
-    parser.add_argument('--resnet_image_size', default=256, help='Size of images for the Resnet model', type=int)
+    parser.add_argument('--iterations', default=100, help='Number of optimization steps for each batch', type=int)
     parser.add_argument('--lr', default=0.02, help='Learning rate for perceptual model', type=float)
     parser.add_argument('--decay_rate', default=0.9, help='Decay rate for learning rate', type=float)
-    parser.add_argument('--iterations', default=100, help='Number of optimization steps for each batch', type=int)
     parser.add_argument('--decay_steps', default=10, help='Decay steps for learning rate decay (as a percent of iterations)', type=float)
-    parser.add_argument('--load_effnet', default='data/finetuned_effnet.h5', help='Model to load for EfficientNet approximation of dlatents')
-    parser.add_argument('--use_resnet', default=True, help='Use pretrained ResNet for approximating dlatents', type=lambda x: (str(x).lower() == 'true'))
+    parser.add_argument('--image_size', default=256, help='Size of images for perceptual model', type=int)
+    parser.add_argument('--resnet_image_size', default=256, help='Size of images for the Resnet model', type=int)
 
     # Loss function options
     parser.add_argument('--use_vgg_loss', default=0.4, help='Use VGG perceptual loss; 0 to disable, > 0 to scale.', type=float)
@@ -56,27 +54,9 @@ def main():
     parser.add_argument('--tile_dlatents', default=False, help='Tile dlatents to use a single vector at each scale', type=bool)
     parser.add_argument('--clipping_threshold', default=2.0, help='Stochastic clipping of gradient values outside of this threshold', type=float)
 
-    # Masking params -> DEGAGER
-    parser.add_argument('--load_mask', default=False, help='Load segmentation masks', type=bool)
-    parser.add_argument('--face_mask', default=False, help='Generate a mask for predicting only the face area', type=bool)
-    parser.add_argument('--use_grabcut', default=True, help='Use grabcut algorithm on the face mask to better segment the foreground', type=bool)
-    parser.add_argument('--scale_mask', default=1.5, help='Look over a wider section of foreground for grabcut', type=float)
-
-    # Video params   -> DEGAGER
-    parser.add_argument('--video_dir', default='videos', help='Directory for storing training videos')
-    parser.add_argument('--output_video', default=False, help='Generate videos of the optimization process', type=bool)
-    parser.add_argument('--video_codec', default='MJPG', help='FOURCC-supported video codec name')
-    parser.add_argument('--video_frame_rate', default=24, help='Video frames per second', type=int)
-    parser.add_argument('--video_size', default=512, help='Video size in pixels', type=int)
-    parser.add_argument('--video_skip', default=1, help='Only write every n frames (1 = write every frame)', type=int)
-
     args, other_args = parser.parse_known_args()
 
-    args.decay_steps *= 0.01 * args.iterations # Calculate steps as a percent of total iterations
-
-    if args.output_video:
-      import cv2
-      synthesis_kwargs = dict(output_transform=dict(func=tflib.convert_images_to_uint8, nchw_to_nhwc=False), minibatch_size=args.batch_size)
+    args.decay_steps *= 0.01 * args.iterations  # Calculate steps as a percent of total iterations
 
     ref_images = [os.path.join(args.src_dir, x) for x in os.listdir(args.src_dir)]
     ref_images = list(filter(os.path.isfile, ref_images))
@@ -84,95 +64,64 @@ def main():
     if len(ref_images) == 0:
         raise Exception('%s is empty' % args.src_dir)
 
-    os.makedirs(args.data_dir, exist_ok=True)
-    os.makedirs(args.mask_dir, exist_ok=True)
+    # Create output directories
+    os.makedirs('data', exist_ok=True)
     os.makedirs(args.generated_images_dir, exist_ok=True)
     os.makedirs(args.guessed_images_dir, exist_ok=True)
     os.makedirs(args.dlatent_dir, exist_ok=True)
-    os.makedirs(args.video_dir, exist_ok=True)
 
     # Initialize generator and perceptual model
     tflib.init_tf()
-    with open_url(args.model_url, cache_dir='cache') as f:
+    with open_url(url_styleGAN, cache_dir='cache') as f:
         generator_network, discriminator_network, Gs_network = pickle.load(f)
 
-    generator = Generator(Gs_network, args.batch_size, clipping_threshold=args.clipping_threshold, tiled_dlatent=args.tile_dlatents, model_res=args.model_res, randomize_noise=args.randomize_noise)
-    if (args.dlatent_avg != ''):
-        generator.set_dlatent_avg(np.load(args.dlatent_avg))
+    generator = Generator(model=Gs_network,
+                          batch_size=args.batch_size,
+                          clipping_threshold=args.clipping_threshold,
+                          tiled_dlatent=args.tile_dlatents,
+                          model_res=args.model_res,
+                          randomize_noise=args.randomize_noise)
 
     perc_model = None
-    if (args.use_lpips_loss > 0.00000001):
+    if (args.use_lpips_loss > 1e-7):
         with open_url(url_VGG_perceptual, cache_dir='cache') as f:
             perc_model = pickle.load(f)
     perceptual_model = PerceptualModel(args, perc_model=perc_model, batch_size=args.batch_size)
     perceptual_model.build_perceptual_model(generator)
 
-    ff_model = None
-
     # Optimize (only) dlatents by minimizing perceptual loss between reference and generated images in feature space
-    for images_batch in tqdm(split_to_batches(ref_images, args.batch_size), total=len(ref_images)//args.batch_size):
+    for images_batch in tqdm(split_to_batches(ref_images, args.batch_size), total=len(ref_images) // args.batch_size):
         names = [os.path.splitext(os.path.basename(x))[0] for x in images_batch]
-        if args.output_video:
-          video_out = {}
-          for name in names:
-            video_out[name] = cv2.VideoWriter(os.path.join(args.video_dir, f'{name}.avi'),cv2.VideoWriter_fourcc(*args.video_codec), args.video_frame_rate, (args.video_size,args.video_size))
-
         perceptual_model.set_reference_images(images_batch)
-        dlatents = None
-        if (args.load_last != ''): # load previous dlatents for initialization
-            for name in names:
-                dl = np.expand_dims(np.load(os.path.join(args.load_last, f'{name}.npy')),axis=0)
-                if (dlatents is None):
-                    dlatents = dl
-                else:
-                    dlatents = np.vstack((dlatents,dl))
-        else:
-            if (ff_model is None):
-                if (args.use_resnet):
-                    print("\nLoading ResNet Model:")
-                    resnet_model_fn = 'data/finetuned_resnet.h5'
-                    gdown.download(url_resnet, resnet_model_fn, quiet=False)
-                    ff_model = load_model(resnet_model_fn)
-                    from keras.applications.resnet50 import preprocess_input
-            if (ff_model is None):
-                if os.path.exists(args.load_effnet):
-                    print("Loading EfficientNet Model:")
-                    ff_model = load_model(args.load_effnet)
-                    from efficientnet import preprocess_input
-            if (ff_model is not None): # predict initial dlatents with ResNet model
-                dlatents = ff_model.predict(preprocess_input(load_images(images_batch,image_size=args.resnet_image_size)))
-        if dlatents is not None:
+
+        # predict initial dlatents with ResNet model
+        if (args.use_resnet):
+            print("\nLoading ResNet Model:")
+            resnet_model_fn = 'data/finetuned_resnet.h5'
+            gdown.download(url_resnet, resnet_model_fn, quiet=True)
+            resnet_model = load_model(resnet_model_fn)
+            dlatents = resnet_model.predict(preprocess_input(load_images(images_batch, image_size=args.resnet_image_size)))
             generator.set_dlatents(dlatents)
 
-        # Generate images from initial dlatents and save them
+        # Generate and save initially guessed images
         initial_dlatents = generator.get_dlatents()
         initial_images = generator.generate_images()
         for img_array, dlatent, img_name in zip(initial_images, initial_dlatents, names):
             img = PIL.Image.fromarray(img_array, 'RGB')
             img.save(os.path.join(args.guessed_images_dir, f'{img_name}.png'), 'PNG')
 
+        # Optimization process to find best latent vectors
         op = perceptual_model.optimize(generator.dlatent_variable, iterations=args.iterations)
-        pbar = tqdm(op, leave=False, total=args.iterations)
-        vid_count = 0
+        progress_bar = tqdm(op, leave=False, total=args.iterations)
         best_loss = None
         best_dlatent = None
-        for loss_dict in pbar:
-            pbar.set_description(" ".join(names) + ": " + "; ".join(["{} {:.4f}".format(k, v)
-                    for k, v in loss_dict.items()]))
+        for loss_dict in progress_bar:
+            progress_bar.set_description(" ".join(names) + ": " + "; ".join(["{} {:.4f}".format(k, v) for k, v in loss_dict.items()]))
             if best_loss is None or loss_dict["loss"] < best_loss:
                 best_loss = loss_dict["loss"]
                 best_dlatent = generator.get_dlatents()
-            if args.output_video and (vid_count % args.video_skip == 0):
-              batch_frames = generator.generate_images()
-              for i, name in enumerate(names):
-                video_frame = PIL.Image.fromarray(batch_frames[i], 'RGB').resize((args.video_size,args.video_size),PIL.Image.LANCZOS)
-                video_out[name].write(cv2.cvtColor(np.array(video_frame).astype('uint8'), cv2.COLOR_RGB2BGR))
             generator.stochastic_clip_dlatents()
         print(" ".join(names), " Loss {:.4f}".format(best_loss))
-
-        if args.output_video:
-            for name in names:
-                video_out[name].release()
 
         # Generate images from found dlatents and save them
         generator.set_dlatents(best_dlatent)
